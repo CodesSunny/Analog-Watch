55332c7f4e7e89b5390f1942d5fa10f7d44d602d
TechSunny <sunnymotivation2@gmail.com>
2024-10-08 22:28:50 +0530

    timer display code added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+window.onload=()=>{
 const updateClock = () => {
     const date = new Date(); // Get the current time
 
@@ -20,3 +21,76 @@ const updateClock = () => {
 // Update the clock every second
 setInterval(updateClock, 1000);
 updateClock(); // Initial call to set the correct position immediately
+
+
+// display day ,time
+const displayArea = document.querySelector(".time-display");
+const showDay = document.querySelector(".day-display");
+const showHour = document.querySelector(".hr-display");
+const showMin = document.querySelector(".min-display");
+const showSec = document.querySelector(".sec-display");
+const showSession = document.querySelector(".session-display");
+
+
+let displayTime = ()=>{  
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours(); 
+    const currentMin = currentTime.getMinutes()
+    const currentSec = currentTime.getSeconds()
+    showToDay(currentTime);
+    displayHr(currentHour);
+    displayMin(currentMin);
+    displaySec(currentSec);
+}
+
+
+function showToDay(currentTime){
+    const currentDay =  currentTime.getDay();
+    if(currentDay == 0){
+        showDay.textContent = "Sun";
+    }else if(currentDay == 1){
+        showDay.textContent = "Mon";
+    }else if(currentDay == 2){
+        showDay.textContent = "Tue";
+    }else if(currentDay == 3){
+        showDay.textContent = "Wed";
+    }else if(currentDay == 4){
+        showDay.textContent = "Thu";
+    }else if(currentDay == 5){
+        showDay.textContent = "Fri";
+    }else if(currentDay == 6){
+        showDay.textContent = "Sat";
+    }
+}
+
+function displayHr(currentHour){
+    if(currentHour > 12){
+        currentHour -= 12;
+        showHour.textContent = currentHour +":"; 
+        showSession.textContent = "PM";
+    }else if(currentHour == 12){
+    }else{
+        showSession.textContent = "AM";
+    }
+}
+
+function displayMin(currentMin){
+    if(currentMin < 10){
+        showMin.textContent = "0" + currentMin +":";    
+    }else{
+        showMin.textContent = currentMin +":";
+    }
+}
+
+function displaySec(currentSec){
+    if(currentSec < 10){
+        showSec.textContent = "0" + currentSec;    
+    }else{
+        showSec.textContent = currentSec;
+    }
+}
+
+// Update time display every second
+setInterval(displayTime, 1000);
+displayTime();  //intial call
+}
